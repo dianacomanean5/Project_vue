36504447aec793fde7c8953672cef78da845ff18
isabela.lacatar <isabela.lacatar@yahoo.com>
2020-04-24 14:21:54 +0300

    added model and crudform(need corrections)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <CrudForm v-bind:users="users"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Vue from 'vue'
+    import Vuelidate from 'vuelidate'
+    import CrudForm from './components/CrudForm.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    Vue.use(Vuelidate);
+    import {required, minLength, maxLength} from 'vuelidate/lib/validators'
+
+    export default {
+      name: 'App',
+      components: {
+        CrudForm
+      },
+      data() {
+        return {
+          users: [{
+            id: 0,
+            name: 'Maria',
+            age: '30',
+            city: 'Sibiu',
+            email: 'isabela@yahoo.com',
+            number: '0740051205'
+          }]
+        }
+      },
+      validations: {
+        name: {
+          required: true,
+          type: String
+        },
+        age: {
+          required: true,
+          minLength: minLength(1),
+        },
+        city: {
+          required,
+
+        },
+        email: {
+          required,
+          message: "Email must be name@example.com"
+        },
+        number: {
+          required,
+          maxLength: maxLength(20)
+        }
+      },
+    }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/CrudForm.vue
+++ src/components/CrudForm.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="hello">
+    <div>
+
     </div>
 </template>
 
 <script>
     export default {
-        name: 'HelloWorld',
+        name: 'Model',
         props: {
-            msg: String
+            users:
+                {type: Array}
         }
     }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     h3 {
         margin: 40px 0 0;
