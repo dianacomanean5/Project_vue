a6b4f7c440814daece54378c0cab49113532fb0d
diana.comanean <dianacomanean@gmail.com>
2020-04-28 14:24:13 +0300

    creating grid table

--- src/components/GridCrud.vue
+++ src/components/GridCrud.vue
@@ -1,57 +1,81 @@
 <template>
-    <div>
-        <table border='1' width='80%' style='border-collapse: collapse;' class="table table-striped table-dark">
+    <div id="demo">
+        <table border='1' width='80%' style='border-collapse: collapse;' class="center">
             <thead>
+
+
             <tr>
                 <th>Id</th>
                 <th>Name</th>
                 <th>Age</th>
-                <th>City</th>
+                <th>Skills</th>
                 <th>Email</th>
-                <th>Number</th>
+                <th>Status</th>
+
             </tr>
+
             </thead>
             <tbody>
-            <tr v-for="(user,index) in this.users"  v-bind:key="index">
-                <td>{{ user.id }}</td>
-                <td>{{ user.name }}</td>
-                <td>{{ user.age }}</td>
-                <td>{{ user.city }}</td>
-                <td>{{ user.email}}</td>
-                <td>{{ user.number}}</td>
+
+            <tr v-for="(model,key) in this.model" v-bind:key="key" >
+                <td>{{ model.id }}</td>
+                <td>{{ model.name }}</td>
+                <td>{{ model.age }}</td>
+                <td>{{ model.skills }}</td>
+                <td>{{ model.email}}</td>
+                <td>{{ model.status}}</td>
 
             </tr>
 
             </tbody>
         </table>
-        <button type="button" class="btn btn-primary btn-lg">Add</button>
+
     </div>
 </template>
-
 <script>
+    // register the grid component
     export default {
-        name: 'Model',
+        // template: "#grid-template",
+
         props:
-            ['users']
+            ['model']
+
 
     }
 </script>
-<style scoped>
-    h3 {
-        margin: 40px 0 0;
+<style>
+    table {
+        font-family: 'Open Sans', sans-serif;
+        width: 750px;
+        border-collapse: collapse;
+        border: 3px solid #44475C;
+        margin: 10px 10px 0 10px;
+
     }
 
-    ul {
-        list-style-type: none;
-        padding: 0;
+    table th {
+        text-transform: uppercase;
+        text-align: center;
+        background: #44475C;
+        color: #FFF;
+        padding: 8px;
+        min-width: 30px;
     }
 
-    li {
-        display: inline-block;
-        margin: 0 10px;
+    table td {
+        text-align: center;
+        padding: 8px;
+        border-right: 2px solid #7D82A8;
     }
+    table td:last-child {
+        border-right: none;
+    }
+    table tbody tr:nth-child(2n) td {
+        background: #D4D8F9;
 
-    a {
-        color: #42b983;
+    }
+    table.center {
+        margin-left:auto;
+        margin-right:auto;
     }
 </style>
