2b1560610cff75b6169f0e22e443b7090abfaf8d
isabela.lacatar <isabela.lacatar@yahoo.com>
2020-04-28 12:29:22 +0300

    added crudform(corrections part3)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
     <div id="app">
-        <CrudForm />
+        <CrudForm :schema="schema" :model="model" :options="formOptions"/>
 
     </div>
 </template>
 
 <script>
 
+    import Vue from 'vue';
+    import VueFormGenerator from 'vue-form-generator';
+    import 'vue-form-generator/dist/vfg.css';
+    import Vuetify from 'vuetify'
+    import 'vuetify/dist/vuetify.min.css'
+    Vue.use(VueFormGenerator);
+    Vue.use(Vuetify);
     import CrudForm from './components/CrudForm.vue';
-
 
     export default {
         name: 'App',
@@ -18,6 +24,81 @@
         },
         data() {
             return {
+                model: {
+                    id: 1,
+                    name: 'Maria',
+                    age: '18',
+                    skills: ["HTML5", "Javascript", "CSS3", "VueJS"],
+                    email: 'maria@yahoo.com',
+                    status: true
+                },
+                schema: {
+                    fields: [
+                        {
+                            type: "input",
+                            inputType: "text",
+                            label: "ID",
+                            model: "id",
+                            readonly: true,
+                            featured: false,
+                            disabled: true
+                        },
+                        {
+                            type: 'input',
+                            inputType: 'text',
+                            label: 'Name',
+                            model: 'name',
+                            placeholder: 'Your name',
+                            featured: true,
+                            required: true,
+                            validator: VueFormGenerator.validators.string
+                        },
+                        {
+                            type: "input",
+                            inputType: "number",
+                            label: "Age",
+                            model: "age",
+                            min: 18,
+                            // validator: VueFormGenerator.validators.number
+                        },
+                        {
+                            type: "checklist",
+                            label: "Skills",
+                            model: "skills",
+                            multi: true,
+                            required: true,
+                            multiSelect: true,
+                            values: ["HTML5", "Javascript", "CSS3", "CoffeeScript", "AngularJS", "ReactJS", "VueJS"]
+                        },
+                        {
+                            type: 'input',
+                            inputType: 'email',
+                            label: 'E-mail',
+                            model: 'email',
+                            required: true,
+                            placeholder: 'User\'s e-mail address',
+                            validator: VueFormGenerator.validators.email
+                        },
+                        {
+                            type: "switch",
+                            label: "Status",
+                            model: "status",
+                            multi: true,
+                            readonly: false,
+                            featured: false,
+                            disabled: false,
+                            default: true,
+                            textOn: "Active",
+                            textOff: "Inactive"
+                        }
+                    ]
+                },
+                formOptions: {
+                    validateAfterLoad: true,
+                    validateAfterChanged:
+                        true
+                }
+
             }
         }
     }
